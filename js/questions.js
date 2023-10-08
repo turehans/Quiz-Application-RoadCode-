ccf05ac1da97f07bf0bdbd598dfe668bd993ea1e
ture.hansson <ture.hansson@aquinas.school.nz>
2023-10-09 10:26:15 +1300

    Adding everything to the repo

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does a yellow reflector (cat's eye) placed on the road mean?",
+    answer: "No Passing",
+    options: [
+      "Fire Hydrant",
+      "No Passing",
+      "Yellow Marks the left side of the road",
+      "Safe Passing"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When coming to a pedestrian crossing with a raised traffic island in the middle, what must you do?",
+    answer: "Only stop and give way if a pedestrian is on your half of the crossing.",
+    options: [
+      "Only stop and give way if a pedestrian is on your half of the crossing.",
+      "Stop and give way if a pedestrian is on any part of the crossing.",
+      "Don't Know",
+      "Either option is fine"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If you are driving a car, what should you do when following another vehicle in wet conditions?",
+    answer: "Observe the four-second rule",
+    options: [
+      "Observe the two-second rule",
+      "Observe the four-second rule",
+      "Observe the six-second rule",
+      "Try to overtake"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When are you allowed to drive across a cycle lane?",
+    answer: "All of the above",
+    options: [
+      "To turn into a driveway",
+      "To turn into a side road",
+      "To park",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "When are you likely to see a slow vehicle bay?",
+    answer: "On a hill",
+    options: [
+      "Approaching a roundabout",
+      "On a motorway",
+      "On a hill",
+      "In the central city"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the maximum speed you may drive if you have a space saver wheel fitted?",
+    answer: "80km/h",
+    options: [
+      "70km/h",
+      "80km/h",
+      "90km/h",
+      "100km/h"
+    ]
+  },
+  {
+    numb: 7,
+    question: "When you are turning to the right:",
+    answer: "Give three seconds' warning",
+    options: [
+      "Give three seconds' warning",
+      "Give two seconds' warning",
+      "Only signal if there are other cars behind or ahead of you",
+      "You Don't need to signal"
+    ]
+  },
+  {
+    numb: 8,
+    question: "For the purpose of applying the Give Way rule, how should an entrance way into a public car park be treated?",
+    answer: "As an intersection",
+    options: [
+      "As an intersection",
+      "The vehicle that is there first has right of way",
+      "As a one-way street",
+      "As a pedestrian crossing"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Before making a lane change to your left, you must signal for at least three seconds. What else must you do?",
+    answer: "Check your blind spots",
+    options: [
+      "Check your blind spots",
+      "Slow down",
+      "Speed up",
+      "Use your brake lights to warn other drivers"
+    ]
+  },
+    {
+      numb: 10,
+      question: "Who can put a blue accident sign up?",
+      answer: "A police officer",
+      options: [
+        "An ambulance officer",
+        "A police officer",
+        "A council officer",
+        "A member of the public"
+     ]
+    },
+
+
+];
